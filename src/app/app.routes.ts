c430a88d06bf081fe281de26d6f0d7c150581a6b
Rohit Kumar Mahor <rk85783@gmail.com>
2026-03-20 21:56:55 +0530

    nested routes - done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // 
+    // { path: 'first-component', component: FirstComponent },
+    // { path: 'second-component', component: SecondComponent },
+    // { path: '', redirectTo: '/first-component', pathMatch: "full" },
+    // { path: '**', component: PageNotFoundComponent },
+
+    // 
+    {
+        path: 'first-component',
+        component: FirstComponent, // this is the component with the <router-outlet> in the template
+        children: [
+            {
+                path: 'child-a', // child route path
+                component: ChildAComponent, // child route component that the router renders
+            },
+            {
+                path: 'child-b',
+                component: ChildBComponent, // another child route component that the router renders
+            },
+        ],
+    },
+    { path: 'second-component', component: SecondComponent },
+    { path: '**', component: PageNotFoundComponent },
+];
